3da02395e99dbff35c0b538757f63a8c2689e381
Andreas Trütschel <andreas@truetschel.net>
2018-02-25 18:19:06 +0100

    Added EndPointRoute doc

--- src/AI4E.Abstractions/Modularity/EndPointRoute.cs
+++ src/AI4E.Abstractions/Modularity/EndPointRoute.cs
@@ -1,4 +1,14 @@
-﻿/* License
+﻿/* Summary
+ * --------------------------------------------------------------------------------------------------------------------
+ * Filename:        EndPointRoute.cs 
+ * Types:           AI4E.Modularity.EndPointRoute
+ * Version:         1.0
+ * Author:          Andreas Trütschel
+ * Last modified:   25.02.2018 
+ * --------------------------------------------------------------------------------------------------------------------
+ */
+
+/* License
  * --------------------------------------------------------------------------------------------------------------------
  * This file is part of the AI4E distribution.
  *   (https://github.com/AI4E/AI4E)
@@ -23,19 +33,32 @@ using System.Collections.Concurrent;
 
 namespace AI4E.Modularity
 {
+    /// <summary>
+    /// Represents the route to a virtual end point. (I.e. its name)
+    /// </summary>
     public sealed class EndPointRoute : IEquatable<EndPointRoute>
     {
         private static readonly ConcurrentDictionary<string, EndPointRoute> _routes = new ConcurrentDictionary<string, EndPointRoute>();
 
         private string _route;
 
+        // TODO: private -> Fix serialization
         public EndPointRoute(string route)
         {
             _route = route;
         }
 
+        /// <summary>
+        /// Gets a stringified version of route.
+        /// </summary>
         public string Route => _route;
 
+        /// <summary>
+        /// Creates a new route from the specified string.
+        /// </summary>
+        /// <param name="route">The string that specifies the route.</param>
+        /// <returns>The created route.</returns>
+        /// <exception cref="ArgumentNullOrWhiteSpaceException">Thrown if <paramref name="route"/> is either null, an empty string or a string consisting of whitespace only.</exception>
         public static EndPointRoute CreateRoute(string route)
         {
             if (string.IsNullOrWhiteSpace(route))
@@ -46,6 +69,11 @@ namespace AI4E.Modularity
             return _routes.GetOrAdd(route, _ => new EndPointRoute(route));
         }
 
+        /// <summary>
+        /// Returns a boolean value indicating whether the specifies route equals the current instance.
+        /// </summary>
+        /// <param name="other">The route to compare to.</param>
+        /// <returns>True if <paramref name="other"/> equals the current route, false otherwise.</returns>
         public bool Equals(EndPointRoute other)
         {
             if (other is null)
@@ -57,21 +85,40 @@ namespace AI4E.Modularity
             return other._route == _route;
         }
 
+        /// <summary>
+        /// Return a boolean value indicating whether the specifies object equals the current route.
+        /// </summary>
+        /// <param name="obj">The object to compare to.</param>
+        /// <returns>True if <paramref name="obj"/> is of type <see cref="EndPointRoute"/> and equals the current route, false otherwise.</returns>
         public override bool Equals(object obj)
         {
             return obj is EndPointRoute route && Equals(route);
         }
 
+        /// <summary>
+        /// Returns a hash code for the current instance.
+        /// </summary>
+        /// <returns>The generated hash code.</returns>
         public override int GetHashCode()
         {
             return _route.GetHashCode();
         }
 
+        /// <summary>
+        /// Returns a stringified version of the route.
+        /// </summary>
+        /// <returns>A string representing the current route.</returns>
         public override string ToString()
         {
             return _route;
         }
 
+        /// <summary>
+        /// Returns a boolean value indicating whether two routes are equal.
+        /// </summary>
+        /// <param name="left">The first route.</param>
+        /// <param name="right">The second route.</param>
+        /// <returns>True if <paramref name="left"/> equals <paramref name="right"/>, false otherwise.</returns>
         public static bool operator ==(EndPointRoute left, EndPointRoute right)
         {
             if (left is null)
@@ -80,6 +127,12 @@ namespace AI4E.Modularity
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Returns a boolean value indicating whether two routes are inequal.
+        /// </summary>
+        /// <param name="left">The first route.</param>
+        /// <param name="right">The second route.</param>
+        /// <returns>True if <paramref name="left"/> does not equal <paramref name="right"/>, false otherwise.</returns>
         public static bool operator !=(EndPointRoute left, EndPointRoute right)
         {
             if (left is null)
