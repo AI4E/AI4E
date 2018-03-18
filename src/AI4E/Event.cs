fb9f376d3f5591e4cfb3fc4b67e6657fc6191fb2
Andreas Trütschel <andreas@truetschel.net>
2018-03-18 12:03:38 +0100

    Added event base type

---
+++ src/AI4E/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AI4E
+{
+    public abstract class Event<TId>
+    {
+        public Event(TId id)
+        {
+            Id = id;
+        }
+
+        public TId Id { get; }
+    }
+
+    public abstract class Event : Event<Guid>
+    {
+        public Event(Guid id) : base(id) { }
+    }
+}
