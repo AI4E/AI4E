80ff220411d831b4a39b590d5de54a5172e3e75f
Andreas Trütschel <andreas@truetschel.net>
2018-04-11 18:45:56 +0200

    Added dictionary extension (Remove method that outputs the removed value)

---
+++ src/AI4E/Internal/DictionaryExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace AI4E.Internal
+{
+    public static class DictionaryExtension
+    {
+        public static bool Remove<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, out TValue value)
+        {
+            if (dictionary is ConcurrentDictionary<TKey, TValue> concurrentDictionary)
+                return concurrentDictionary.TryRemove(key, out value);
+
+            if (dictionary.ContainsKey(key))
+            {
+                value = dictionary[key];
+                dictionary.Remove(key);
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+    }
+}
