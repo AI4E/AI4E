67442d15f9b714e969db645dce7c7b0b286f9e58
Christoph Neuner <christoph_neuner@web.de>
2018-06-03 12:01:26 +0200

    set url

--- samples/AI4E.SignalR.Server/Program.cs
+++ samples/AI4E.SignalR.Server/Program.cs
@@ -19,7 +19,8 @@ namespace AI4E.SignalR.Server
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5002");
           
     }
 }
