8b339a968ea4257e2e756d7e980265e1b6bbdbfe
Andreas Trütschel <andreas@truetschel.net>
2018-06-05 13:14:17 +0200

    Added in memory database

---
+++ test/AI4E.Storage.Transactions.StressTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AI4E.Storage.Transactions.StressTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
