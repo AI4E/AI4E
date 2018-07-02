69a4c13d89ffbee054222981d161a29cdeacd913
Andreas Trütschel <andreas@truetschel.net>
2018-07-02 20:54:44 +0200

    Added initial prototype

---
+++ samples/AI4E.Modularity.Hosting.Sample/Controllers/ModulesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI4E.Modularity.Hosting.Sample.Controllers
+{
+    public sealed class ModulesController : Controller
+    {
+        private readonly IMessageDispatcher _messageDispatcher;
+
+        public ModulesController(IMessageDispatcher messageDispatcher)
+        {
+            _messageDispatcher = messageDispatcher;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return null;
+        }
+    }
+}
---
+++ samples/AI4E.Modularity.Hosting.Sample/Startup.cs
@@ -0,0 +1,63 @@
+using AI4E.Modularity.Host;
+using AI4E.Storage;
+using AI4E.Storage.Domain;
+using AI4E.Storage.InMemory;
+using AI4E.Storage.MongoDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AI4E.Modularity.Hosting.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddStorage()
+                    .UseInMemoryDatabase()
+                    .UseDomainStorage();
+
+            services.AddModularity();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseModularity();
+
+            app.UseMvc();
+        }
+    }
+}
