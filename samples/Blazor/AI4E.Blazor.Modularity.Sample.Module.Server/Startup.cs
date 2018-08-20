c947ba211799aa09ac5ca8463ad2f223af0f3e27
Andreas Trütschel <andreas@truetschel.net>
2018-08-20 19:25:37 +0200

    Added server side blazor module assembly provider

--- samples/Blazor/AI4E.Blazor.Modularity.Sample.Module.Server/Startup.cs
+++ samples/Blazor/AI4E.Blazor.Modularity.Sample.Module.Server/Startup.cs
@@ -1,18 +1,14 @@
 using System.Linq;
 using System.Net.Mime;
-using AI4E.Blazor.Modularity.Sample.Module.App;
+using AI4E.Blazor.Server;
 using AI4E.Modularity.Module;
 using AI4E.Storage;
 using AI4E.Storage.MongoDB;
 using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Net.Http.Headers;
 
 namespace AI4E.Blazor.Modularity.Sample.Module.Server
 {
@@ -46,8 +42,6 @@ namespace AI4E.Blazor.Modularity.Sample.Module.Server
                 options.UseDebugConnection = true;
                 options.DebugConnection = "localhost:8080";
             });
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,48 +56,7 @@ namespace AI4E.Blazor.Modularity.Sample.Module.Server
             }
 
             app.UseStaticFiles();
-
-            var appPath = @"C:\Users\at\Source\Repos\AI4E_feature_blazor\artifacts\bin\AI4E.Blazor.Modularity.Sample.Module.App\Debug\netstandard2.0";
-
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(appPath),
-                ContentTypeProvider = CreateContentTypeProvider(/*config.EnableDebugging*/true),
-                OnPrepareResponse = SetCacheHeaders
-            });
-        }
-
-        private static IContentTypeProvider CreateContentTypeProvider(bool enableDebugging)
-        {
-            var result = new FileExtensionContentTypeProvider();
-            result.Mappings.Add(".dll", MediaTypeNames.Application.Octet);
-            result.Mappings.Add(".mem", MediaTypeNames.Application.Octet);
-            result.Mappings.Add(".wasm", WasmMediaTypeNames.Application.Wasm);
-
-            if (enableDebugging)
-            {
-                result.Mappings.Add(".pdb", MediaTypeNames.Application.Octet);
-            }
-
-            return result;
-        }
-
-        private static void SetCacheHeaders(StaticFileResponseContext ctx)
-        {
-            // By setting "Cache-Control: no-cache", we're allowing the browser to store
-            // a cached copy of the response, but telling it that it must check with the
-            // server for modifications (based on Etag) before using that cached copy.
-            // Longer term, we should generate URLs based on content hashes (at least
-            // for published apps) so that the browser doesn't need to make any requests
-            // for unchanged files.
-            var headers = ctx.Context.Response.GetTypedHeaders();
-            if (headers.CacheControl == null)
-            {
-                headers.CacheControl = new CacheControlHeaderValue
-                {
-                    NoCache = true
-                };
-            }
+            app.UseBlazorModule<App._ViewImports>();
         }
     }
 }
---
+++ src/Blazor/AI4E.Blazor.Server/BlazorConfig.cs
@@ -0,0 +1,87 @@
+﻿/* License
+ * --------------------------------------------------------------------------------------------------------------------
+ * This file is part of the AI4E distribution.
+ *   (https://github.com/AI4E/AI4E)
+ * Copyright (c) 2018 Andreas Truetschel and contributors.
+ * 
+ * AI4E is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation, version 3.
+ *
+ * AI4E is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * --------------------------------------------------------------------------------------------------------------------
+ */
+
+/* Based on
+* --------------------------------------------------------------------------------------------------------------------
+* Asp.Net Blazor
+* Copyright (c) .NET Foundation. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+* these files except in compliance with the License. You may obtain a copy of the
+* License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed
+* under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+* CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+* --------------------------------------------------------------------------------------------------------------------
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AI4E.Blazor.Server
+{
+    internal class BlazorConfig
+    {
+        public string SourceMSBuildPath { get; }
+        public string SourceOutputAssemblyPath { get; }
+        public string WebRootPath { get; }
+        public string DistPath => Path.Combine(Path.GetDirectoryName(SourceOutputAssemblyPath), "dist");
+        public bool EnableAutoRebuilding { get; }
+        public bool EnableDebugging { get; }
+
+        public static BlazorConfig Read(string assemblyPath)
+        {
+            return new BlazorConfig(assemblyPath);
+        }
+
+        private BlazorConfig(string assemblyPath)
+        {
+            // TODO: Instead of assuming the lines are in a specific order, either JSON-encode
+            // the whole thing, or at least give the lines key prefixes (e.g., "reload:<someuri>")
+            // so we're not dependent on order and all lines being present.
+
+            var configFilePath = Path.ChangeExtension(assemblyPath, ".blazor.config");
+            var configLines = File.ReadLines(configFilePath).ToList();
+            SourceMSBuildPath = configLines[0];
+
+            if (SourceMSBuildPath == ".")
+            {
+                SourceMSBuildPath = assemblyPath;
+            }
+
+            var sourceMsBuildDir = Path.GetDirectoryName(SourceMSBuildPath);
+            SourceOutputAssemblyPath = Path.Combine(sourceMsBuildDir, configLines[1]);
+
+            var webRootPath = Path.Combine(sourceMsBuildDir, "wwwroot");
+            if (Directory.Exists(webRootPath))
+            {
+                WebRootPath = webRootPath;
+            }
+
+            EnableAutoRebuilding = configLines.Contains("autorebuild:true", StringComparer.Ordinal);
+            EnableDebugging = configLines.Contains("debug:true", StringComparer.Ordinal);
+        }
+    }
+}
