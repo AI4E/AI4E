929a45494acfff80578433dc2e0f77c83f0a7336
Andreas Trütschel <andreas@truetschel.net>
2018-08-22 07:26:32 +0200

    Added application service initialization to blazor projects

---
+++ src/Blazor/AI4E.Blazor/WebAssemblyHostExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AI4E.Internal;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AI4E.Blazor
+{
+    public static class WebAssemblyHostExtension
+    {
+        public static async Task<IWebAssemblyHost> InitializeApplicationServicesAsync(this IWebAssemblyHost webhost, CancellationToken cancellation = default)
+        {
+            if (webhost == null)
+                throw new ArgumentNullException(nameof(webhost));
+
+            var serviceProvider = webhost.Services;
+            var applicationServiceManager = serviceProvider.GetService<ApplicationServiceManager>();
+
+            if (applicationServiceManager != null)
+            {
+                await applicationServiceManager.InitializeApplicationServicesAsync(serviceProvider, cancellation);
+            }
+
+            return webhost;
+        }
+    }
+}
