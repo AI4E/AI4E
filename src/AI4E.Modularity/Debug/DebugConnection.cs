3474100af3a69e8f8828824e0ac651ff26839ca5
Andreas Trütschel <andreas@truetschel.net>
2018-11-25 16:52:10 +0100

    Refactored debug connection to remove usage of async dispose helper

--- src/AI4E.Modularity/Debug/DebugConnection.cs
+++ src/AI4E.Modularity/Debug/DebugConnection.cs
@@ -7,19 +7,19 @@ using AI4E.Async;
 using AI4E.Internal;
 using AI4E.Proxying;
 using Microsoft.Extensions.Logging;
-using static System.Diagnostics.Debug;
 
 namespace AI4E.Modularity.Debug
 {
-    public sealed class DebugConnection : IAsyncDisposable
+    public sealed class DebugConnection : IDisposable
     {
         private readonly IPEndPoint _debugPort;
         private readonly IDateTimeProvider _dateTimeProvider;
         private readonly IServiceProvider _serviceProvider;
         private readonly ILoggerFactory _loggerFactory;
         private readonly ILogger<DebugConnection> _logger;
-        private readonly AsyncInitializationHelper<(IPEndPoint localAddress, ProxyHost proxyHost)> _initializationHelper;
-        private readonly AsyncDisposeHelper _disposeHelper;
+
+        private readonly DisposableAsyncLazy<TcpClient> _tcpClientLazy;
+        private readonly DisposableAsyncLazy<ProxyHost> _proxyHostLazy;
 
         public DebugConnection(IPEndPoint debugPort, IDateTimeProvider dateTimeProvider, IServiceProvider serviceProvider, ILoggerFactory loggerFactory = null)
         {
@@ -38,92 +38,92 @@ namespace AI4E.Modularity.Debug
             _loggerFactory = loggerFactory;
 
             _logger = _loggerFactory?.CreateLogger<DebugConnection>();
-            _initializationHelper = new AsyncInitializationHelper<(IPEndPoint localAddress, ProxyHost proxyHost)>(InitializeInternalAsync);
-            _disposeHelper = new AsyncDisposeHelper(DisposeInternalAsync);
+
+            _tcpClientLazy = new DisposableAsyncLazy<TcpClient>(
+                factory: CreateTcpClientAsync,
+                disposal: tcpClient => { tcpClient.Dispose(); return Task.CompletedTask; },
+                options: DisposableAsyncLazyOptions.Autostart | DisposableAsyncLazyOptions.ExecuteOnCallingThread);
+
+            _proxyHostLazy = new DisposableAsyncLazy<ProxyHost>(
+                factory: CreateProxyHostAsync,
+                disposal: proxyHost => proxyHost.DisposeAsync(),
+                options: DisposableAsyncLazyOptions.Autostart | DisposableAsyncLazyOptions.ExecuteOnCallingThread);
         }
 
-        public async ValueTask<IPEndPoint> GetLocalAddressAync(CancellationToken cancellation)
+        private async Task<TcpClient> CreateTcpClientAsync(CancellationToken cancellation)
         {
-            var (localAddress, _) = await _initializationHelper.Initialization.WithCancellation(cancellation);
-            return localAddress;
-        }
-
-        public async ValueTask<ProxyHost> GetProxyHostAsync(CancellationToken cancellation)
-        {
-            var (_, proxyHost) = await _initializationHelper.Initialization.WithCancellation(cancellation);
-            return proxyHost;
-        }
-
-        private async Task<(IPEndPoint localAddress, ProxyHost proxyHost)> InitializeInternalAsync(CancellationToken cancellation)
-        {
-            // TODO: Is the tcp client closed on disposal?
-            var tcpClient = new TcpClient(_debugPort.AddressFamily);
-
             _logger?.LogDebug($"Trying to connect to debug port {_debugPort}.");
-
-            var delay = TimeSpan.FromSeconds(1);
-            var maxDelay = TimeSpan.FromSeconds(6);
-            var delayFactor = 1.1;
-
-            while (cancellation.ThrowOrContinue())
+            var tcpClient = new TcpClient(_debugPort.AddressFamily);
+            try
             {
-                try
+                var delay = TimeSpan.FromSeconds(1);
+                var maxDelay = TimeSpan.FromSeconds(6);
+                var delayFactor = 1.1;
+
+                while (cancellation.ThrowOrContinue())
                 {
-                    await tcpClient.ConnectAsync(_debugPort.Address, _debugPort.Port).WithCancellation(cancellation);
-                    break;
-                }
-                catch (SocketException exc) when (exc.SocketErrorCode == SocketError.ConnectionRefused) { }
+                    try
+                    {
+                        await tcpClient.ConnectAsync(_debugPort.Address, _debugPort.Port).WithCancellation(cancellation);
+                        break;
+                    }
+                    catch (SocketException exc) when (exc.SocketErrorCode == SocketError.ConnectionRefused) { }
 
-                _logger?.LogWarning($"Debug port unreachable. Trying again in {delay.TotalSeconds.ToString("0.00")}sec.");
+                    _logger?.LogWarning($"Debug port unreachable. Trying again in {delay.TotalSeconds.ToString("0.00")}sec.");
 
-                await Task.Delay(delay, cancellation);
+                    await Task.Delay(delay, cancellation);
 
-                delay = new TimeSpan((long)(delay.Ticks * delayFactor));
+                    delay = new TimeSpan((long)(delay.Ticks * delayFactor));
 
-                if (delay > maxDelay)
-                {
-                    delay = maxDelay;
+                    if (delay > maxDelay)
+                    {
+                        delay = maxDelay;
+                    }
                 }
             }
+            catch
+            {
+                tcpClient.Dispose();
+                throw;
+            }
 
-            _logger?.LogInformation($"Successfully established connection to debug port.");
+            return tcpClient;
+        }
 
-            var localAddress = tcpClient.Client.LocalEndPoint as IPEndPoint;
+        private async Task<ProxyHost> CreateProxyHostAsync(CancellationToken cancellation)
+        {
+            var tcpClient = await _tcpClientLazy.Task.WithCancellation(cancellation);
+
             var logger = _loggerFactory?.CreateLogger<DisposeAwareStream>();
 
             // TODO: Graceful shutdown
             var stream = new DisposeAwareStream(tcpClient.GetStream(), _dateTimeProvider, () => { Environment.FailFast(""); return Task.CompletedTask; }, logger);
             var proxyHost = new ProxyHost(stream, _serviceProvider);
 
-            return (localAddress, proxyHost);
+            return proxyHost;
         }
 
-        #region Disposal
+        public async ValueTask<IPEndPoint> GetLocalAddressAync(CancellationToken cancellation)
+        {
+            var tcpClient = await _tcpClientLazy.Task.WithCancellation(cancellation);
+            return tcpClient.Client.LocalEndPoint as IPEndPoint;
+        }
 
-        public Task Disposal => _disposeHelper.Disposal;
+        public ValueTask<ProxyHost> GetProxyHostAsync(CancellationToken cancellation)
+        {
+            return new ValueTask<ProxyHost>(_proxyHostLazy.Task.WithCancellation(cancellation));
+        }
 
         public void Dispose()
         {
-            _disposeHelper.Dispose();
-        }
-
-        public Task DisposeAsync()
-        {
-            return _disposeHelper.DisposeAsync();
-        }
-
-        private async Task DisposeInternalAsync()
-        {
-            var (success, (_, proxyHost)) = await _initializationHelper.CancelAsync().HandleExceptionsAsync(_logger);
-
-            if (success)
+            try
             {
-                Assert(proxyHost != null);
-
-                await proxyHost.DisposeAsync();
+                _proxyHostLazy.Dispose();
+            }
+            finally
+            {
+                _tcpClientLazy.Dispose();
             }
         }
-
-        #endregion
     }
 }
