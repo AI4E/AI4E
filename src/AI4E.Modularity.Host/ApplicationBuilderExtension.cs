e166334be18301ea5ce30327e1d63f22785da6b1
Andreas Trütschel <andreas@truetschel.net>
2018-12-07 17:11:18 +0100

    Refactored module host middleware

--- src/AI4E.Modularity.Host/ApplicationBuilderExtension.cs
+++ src/AI4E.Modularity.Host/ApplicationBuilderExtension.cs
@@ -19,108 +19,27 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using AI4E.Internal;
-using AI4E.Routing;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Primitives;
 
 namespace AI4E.Modularity.Host
 {
     public static class ApplicationBuilderExtension
     {
-        private static readonly byte[] _emptyArray = new byte[0];
-
-        public static void UseModularity(this IApplicationBuilder applicationBuilder)
+        public static IApplicationBuilder UseModularity(this IApplicationBuilder builder)
         {
-            if (applicationBuilder == null)
-                throw new ArgumentNullException(nameof(applicationBuilder));
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
 
-            var serviceProvider = applicationBuilder.ApplicationServices;
+            var serviceProvider = builder.ApplicationServices;
 
             if (serviceProvider.GetService<ModularityMarkerService>() == null)
             {
                 throw new InvalidOperationException("Cannot use the modular host without adding the modularity services.");
             }
 
-            // Initialize the module-host.
-            var dispatcher = serviceProvider.GetRequiredService<IRemoteMessageDispatcher>();
-            var pathMapper = serviceProvider.GetRequiredService<IPathMapper>();
-
-            applicationBuilder.Use(async (context, next) =>
-            {
-                var cancellation = context?.RequestAborted ?? default;
-                var endPoint = await pathMapper.MapHttpPathAsync(context.Features.Get<IHttpRequestFeature>().Path.AsMemory(), cancellation);
-
-                if (endPoint != EndPointAddress.UnknownAddress)
-                {
-                    var requestFeature = context.Features.FirstOrDefault(p => p.Key == typeof(IHttpRequestFeature)).Value as IHttpRequestFeature;
-
-                    var moduleHttpRequest = new ModuleHttpRequest
-                    {
-                        Method = requestFeature.Method,
-                        Path = requestFeature.Path,
-                        PathBase = requestFeature.PathBase,
-                        Protocol = requestFeature.Protocol,
-                        QueryString = requestFeature.QueryString,
-                        RawTarget = requestFeature.RawTarget,
-                        Scheme = requestFeature.Scheme,
-                        Body = requestFeature.Body == null ? _emptyArray : await requestFeature.Body.ToArrayAsync(),
-                        Headers = new Dictionary<string, string[]>()
-                    };
-
-                    foreach (var entry in requestFeature.Headers)
-                    {
-                        moduleHttpRequest.Headers.Add(entry.Key, entry.Value.ToArray());
-                    }
-
-                    var message = moduleHttpRequest;
-                    var dispatchResult = await dispatcher.DispatchAsync(new DispatchDataDictionary<ModuleHttpRequest>(message), publish: false, endPoint, cancellation);
-                    var response = default(ModuleHttpResponse);
-
-                    if (dispatchResult.IsSuccess)
-                    {
-                        response = (dispatchResult as IDispatchResult<ModuleHttpResponse>).Result;
-                    }
-
-                    if (response == null)
-                    {
-                        await next?.Invoke();
-                        return;
-                    }
-
-                    var responseFeature = context.Features.FirstOrDefault(p => p.Key == typeof(IHttpResponseFeature)).Value as IHttpResponseFeature;
-
-                    responseFeature.StatusCode = response.StatusCode;
-                    responseFeature.ReasonPhrase = response.ReasonPhrase;
-
-                    responseFeature.Headers.Clear();
-
-                    foreach (var header in response.Headers)
-                    {
-                        responseFeature.Headers.Add(header.Key, new StringValues(header.Value));
-                    }
-
-                    if (response.Body.Length > 0)
-                    {
-                        await responseFeature.Body.WriteAsync(response.Body, 0, response.Body.Length);
-                    }
-
-                    if (responseFeature.StatusCode == 404)
-                    {
-                        await next?.Invoke();
-                        return;
-                    }
-                }
-                else
-                {
-                    await next?.Invoke();
-                }
-            });
+            return builder.UseMiddleware<ModuleHostHttpMiddleware>();
         }
     }
 }
