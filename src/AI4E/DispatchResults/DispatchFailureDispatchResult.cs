20b38c4c949925e8aaf9268699b335dc3f81b5c7
Andreas Trütschel <andreas@truetschel.net>
2019-03-18 17:05:17 +0100

    Added DispatchFailureDispatchResult unit-tests

--- src/AI4E/DispatchResults/DispatchFailureDispatchResult.cs
+++ src/AI4E/DispatchResults/DispatchFailureDispatchResult.cs
@@ -19,17 +19,66 @@
  */
 
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace AI4E.DispatchResults
 {
+    /// <summary>
+    /// Describes the result of a message dispatch operation that failed because the message cannot be dispatched.
+    /// </summary>
     public sealed class DispatchFailureDispatchResult : FailureDispatchResult
     {
-        public DispatchFailureDispatchResult(Type messageType)
-            : base($"The message of type '{(messageType ?? throw new ArgumentNullException(nameof(messageType))).FullName }' cannot be dispatched.")
+        /// <summary>
+        /// Creates a new instance of the <see cref="DispatchFailureDispatchResult"/> type.
+        /// </summary>
+        /// <param name="messageType">The type of message that cannot be dispatched.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="messageType"/> is <c>null</c>.</exception>
+        public DispatchFailureDispatchResult(Type messageType) : base(FormatDefaultMessage(messageType))
         {
             MessageType = messageType;
         }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="DispatchFailureDispatchResult"/> type.
+        /// </summary>
+        /// <param name="messageType">The type of message that cannot be dispatched.</param>
+        /// <param name="message">A message describing the message dispatch result.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if either <paramref name="messageType"/> or <paramref name="message"/> is <c>null</c>.
+        /// </exception>
+        public DispatchFailureDispatchResult(Type messageType, string message) : base(message)
+        {
+            MessageType = messageType;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="DispatchFailureDispatchResult"/> type.
+        /// </summary>
+        /// <param name="messageType">The type of message that cannot be dispatched.</param>
+        /// <param name="message">A message describing the message dispatch result.</param>
+        /// <param name="resultData">A collection of key value pairs that represent additional result data.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if any of <paramref name="messageType"/>, <paramref name="message"/> or <paramref name="resultData"/> is <c>null</c>.
+        /// </exception>
+        [JsonConstructor]
+        public DispatchFailureDispatchResult(Type messageType, string message, IReadOnlyDictionary<string, object> resultData)
+            : base(message, resultData)
+        {
+            MessageType = messageType;
+        }
+
+        /// <summary>
+        /// Gets the type of message that cannot be dispatched.
+        /// </summary>
         public Type MessageType { get; }
+
+        private static string FormatDefaultMessage(Type messageType)
+        {
+            if (messageType == null)
+                throw new ArgumentNullException(nameof(messageType));
+
+            return $"The message of type '{messageType}' cannot be dispatched.";
+        }
     }
 }
