d267667a57d5055ddefdd25b58ddf551844864a2
Andreas Trütschel <andreas@truetschel.net>
2019-06-11 15:11:23 +0200

    Add API documentation for ProjectionTargetDescriptor and removed generic version.

--- src/AI4E.Storage.Projection.Abstractions/ProjectionTargetDescriptor.cs
+++ src/AI4E.Storage.Projection.Abstractions/ProjectionTargetDescriptor.cs
@@ -19,32 +19,55 @@
  */
 
 using System;
-using System.Collections.Generic;
 
 namespace AI4E.Storage.Projection
 {
+    /// <summary>
+    /// Describes a projection target.
+    /// </summary>
     public readonly struct ProjectionTargetDescriptor : IEquatable<ProjectionTargetDescriptor>
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="ProjectionTargetDescriptor"/> type.
+        /// </summary>
+        /// <param name="targetType">The type of the projection target.</param>
+        /// <param name="targetId">The id of the projection target.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if either <paramref name="targetType"/> or <paramref name="targetType"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="targetId"/> is empty or consists of whitespace only.</exception>
         public ProjectionTargetDescriptor(Type targetType, string targetId)
         {
             if (targetType is null)
                 throw new ArgumentNullException(nameof(targetType));
 
-            if (targetId is null || targetId.Equals(default))
-                throw new ArgumentDefaultException(nameof(targetId));
+            if (targetId is null)
+                throw new ArgumentNullException(nameof(targetId));
+
+            if (string.IsNullOrWhiteSpace(targetId))
+                throw new ArgumentException("The value must neither be empty, nor consist of whitespace only.", nameof(targetId));
 
             TargetType = targetType;
             TargetId = targetId;
         }
 
+        /// <summary>
+        /// Gets the type of the projection target.
+        /// </summary>
         public Type TargetType { get; }
+
+        /// <summary>
+        /// Gets the id of the projection target.
+        /// </summary>
         public string TargetId { get; }
 
+        /// <inheritdoc/>
         public override bool Equals(object obj)
         {
             return obj is ProjectionTargetDescriptor entityDescriptor && Equals(entityDescriptor);
         }
 
+        /// <inheritdoc/>
         public bool Equals(ProjectionTargetDescriptor other)
         {
             if (other.TargetType is null && TargetType is null)
@@ -53,6 +76,7 @@ namespace AI4E.Storage.Projection
             return (other.TargetType, other.TargetId) == (TargetType, TargetId);
         }
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             if (TargetType is null)
@@ -61,69 +85,26 @@ namespace AI4E.Storage.Projection
             return (TargetType, TargetId).GetHashCode();
         }
 
+        /// <summary>
+        /// Checks two instances of <see cref="ProjectionTargetDescriptor"/> to be equal.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns>True if <paramref name="left"/> equals <paramref name="right"/>, false otherwise.</returns>
         public static bool operator ==(in ProjectionTargetDescriptor left, in ProjectionTargetDescriptor right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Checks two instances of <see cref="ProjectionTargetDescriptor"/> to be non-equal.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns>True if <paramref name="left"/> does not equal <paramref name="right"/>, false otherwise.</returns>
         public static bool operator !=(in ProjectionTargetDescriptor left, in ProjectionTargetDescriptor right)
         {
             return !left.Equals(right);
-        }
-    }
-
-    public readonly struct ProjectionTargetDescriptor<TId> : IEquatable<ProjectionTargetDescriptor<TId>>
-    {
-        public ProjectionTargetDescriptor(Type targetType, TId targetId)
-        {
-            if (targetType == null)
-                throw new ArgumentNullException(nameof(targetType));
-
-            if (targetId == null || targetId.Equals(default))
-                throw new ArgumentDefaultException(nameof(targetId));
-
-            TargetType = targetType;
-            TargetId = targetId;
-        }
-
-        public Type TargetType { get; }
-        public TId TargetId { get; }
-        public string StringifiedTargetId => TargetId.ToString();
-
-        public override bool Equals(object obj)
-        {
-            return obj is ProjectionTargetDescriptor<TId> entityDescriptor && Equals(entityDescriptor);
-        }
-
-        public bool Equals(ProjectionTargetDescriptor<TId> other)
-        {
-            if (other.TargetType is null && TargetType is null)
-                return true;
-
-            return other.TargetType == TargetType && EqualityComparer<TId>.Default.Equals(other.TargetId, TargetId);
-        }
-
-        public override int GetHashCode()
-        {
-            if (TargetType == null)
-                return 0;
-
-            return (TargetType, TargetId).GetHashCode();
-        }
-
-        public static bool operator ==(in ProjectionTargetDescriptor<TId> left, in ProjectionTargetDescriptor<TId> right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(in ProjectionTargetDescriptor<TId> left, in ProjectionTargetDescriptor<TId> right)
-        {
-            return !left.Equals(right);
-        }
-
-        public static implicit operator ProjectionTargetDescriptor(in ProjectionTargetDescriptor<TId> typedDescriptor)
-        {
-            return new ProjectionTargetDescriptor(typedDescriptor.TargetType, typedDescriptor.StringifiedTargetId);
         }
     }
 }
