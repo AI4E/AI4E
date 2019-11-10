e61f00956ace96fea155845aa678daa9eb403203
Andreas Trütschel <andreas@truetschel.net>
2019-11-10 15:37:30 +0100

    Fork ParameterDefaultValue from .NET Extensions

---
+++ src/Utils/src/AI4E.Utils/ParameterDefaultValue.cs
@@ -0,0 +1,90 @@
+﻿/* License
+ * --------------------------------------------------------------------------------------------------------------------
+ * This file is part of the AI4E distribution.
+ *   (https://github.com/AI4E/AI4E)
+ * Copyright (c) 2018 - 2019 Andreas Truetschel and contributors.
+ * 
+ * AI4E is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation, version 3.
+ *
+ * AI4E is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * --------------------------------------------------------------------------------------------------------------------
+ */
+
+/* Based on
+* ---------------------------------------------------------------------------------------------------------------------
+* .NET Extensions (https://github.com/aspnet/Extensions)
+* Copyright (c) .NET Foundation. All rights reserved.
+* Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+* ---------------------------------------------------------------------------------------------------------------------
+*/
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace AI4E.Utils
+{
+    public static class ParameterDefaultValue
+    {
+        private static readonly Type _nullable = typeof(Nullable<>);
+
+        public static bool TryGetDefaultValue(ParameterInfo parameter, [NotNullWhen(true)] out object? defaultValue)
+        {
+            if (parameter is null)
+                throw new ArgumentNullException(nameof(parameter));
+
+            bool hasDefaultValue;
+            var tryToGetDefaultValue = true;
+            defaultValue = null;
+
+            try
+            {
+                hasDefaultValue = parameter.HasDefaultValue;
+            }
+            catch (FormatException) when (parameter.ParameterType == typeof(DateTime))
+            {
+                // Workaround for https://github.com/dotnet/corefx/issues/12338
+                // If HasDefaultValue throws FormatException for DateTime
+                // we expect it to have default value
+                hasDefaultValue = true;
+                tryToGetDefaultValue = false;
+            }
+
+            if (hasDefaultValue)
+            {
+                if (tryToGetDefaultValue)
+                {
+                    defaultValue = parameter.DefaultValue;
+                }
+
+                // Workaround for https://github.com/dotnet/corefx/issues/11797
+                if (defaultValue == null && parameter.ParameterType.IsValueType)
+                {
+                    defaultValue = Activator.CreateInstance(parameter.ParameterType);
+                }
+
+                // Handle nullable enums
+                if (defaultValue != null &&
+                    parameter.ParameterType.IsGenericType &&
+                    parameter.ParameterType.GetGenericTypeDefinition() == _nullable)
+                {
+                    var underlyingType = Nullable.GetUnderlyingType(parameter.ParameterType);
+                    if (underlyingType != null && underlyingType.IsEnum)
+                    {
+                        defaultValue = Enum.ToObject(underlyingType, defaultValue);
+                    }
+                }
+            }
+
+            return hasDefaultValue;
+        }
+    }
+}
