ea9b2f33c6557e450e535332229129f046afb879
Andreas Trütschel <andreas@truetschel.net>
2020-02-09 18:05:12 +0100

    Add NoDatabase and NoDatabaseScope null objects

---
+++ src/Storage/src/AI4E.Storage/NoDatabase.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AI4E.Storage
+{
+    public sealed class NoDatabase : IDatabase
+    {
+        public static NoDatabase Instance { get; } = new NoDatabase();
+
+        private NoDatabase() { }
+
+        /// <inheritdoc />
+        public bool SupportsScopes => true;
+
+        /// <inheritdoc />
+        public ValueTask<bool> AddAsync<TEntry>(TEntry entry, CancellationToken cancellation)
+            where TEntry : class
+        {
+            if (entry is null)
+                throw new ArgumentNullException(nameof(entry));
+
+            return new ValueTask<bool>(false);
+        }
+
+        /// <inheritdoc />
+        public ValueTask Clear<TEntry>(CancellationToken cancellation)
+            where TEntry : class
+        {
+            return default;
+        }
+
+        /// <inheritdoc />
+        public IDatabaseScope CreateScope()
+        {
+            return new NoDatabaseScope();
+        }
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<TEntry> GetAsync<TEntry>(
+            Expression<Func<TEntry, bool>> predicate,
+            CancellationToken cancellation)
+            where TEntry : class
+        {
+            if (predicate is null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            return AsyncEnumerable.Empty<TEntry>();
+        }
+
+        /// <inheritdoc />
+        public IAsyncEnumerable<TResult> QueryAsync<TEntry, TResult>(
+            Func<IQueryable<TEntry>, IQueryable<TResult>> queryShaper,
+            CancellationToken cancellation)
+            where TEntry : class
+        {
+            if (queryShaper is null)
+                throw new ArgumentNullException(nameof(queryShaper));
+
+            return AsyncEnumerable.Empty<TResult>();
+        }
+
+        /// <inheritdoc />
+        public ValueTask<bool> RemoveAsync<TEntry>(
+            TEntry entry,
+            Expression<Func<TEntry, bool>> predicate,
+            CancellationToken cancellation)
+            where TEntry : class
+        {
+            if (entry is null)
+                throw new ArgumentNullException(nameof(entry));
+
+            if (predicate is null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            return new ValueTask<bool>(false);
+        }
+
+        /// <inheritdoc />
+        public ValueTask<bool> UpdateAsync<TEntry>(
+            TEntry entry,
+            Expression<Func<TEntry, bool>> predicate,
+            CancellationToken cancellation)
+            where TEntry : class
+        {
+            if (entry is null)
+                throw new ArgumentNullException(nameof(entry));
+
+            if (predicate is null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            return new ValueTask<bool>(false);
+        }
+    }
+}
---
+++ src/Storage/test/AI4E.Storage.Test/TestTypes/NoIdEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace AI4E.Storage.Test.TestTypes
+{
+    public class NoIdEntry
+    {
+        public override int GetHashCode()
+        {
+            return 42;
+        }
+    }
+}
