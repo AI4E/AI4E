d40c5856441adadcf41247544da494259dee7660
Andreas Trütschel <andreas@truetschel.net>
2020-06-14 20:36:06 +0200

    Move blazor-wasm projects to labs

---
+++ lab/AspNetCore.Components/samples/Logging.Sample/Logging.Sample.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Logging.Sample.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        private static WebAssemblyHostBuilder CreateHostBuilder(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault();
+            ConfigureServices(builder.Services);
+            builder.RootComponents.Add<App>("app");
+            return builder;
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(builder =>
+            {
+                builder.AddBrowserConsole();
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
+        }
+    }
+}
