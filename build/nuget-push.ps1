1e21ed02cb1199f56700bb44b9bffbd1ca2addcb
Andreas Trütschel <andreas@truetschel.net>
2020-07-14 11:43:40 +0200

    Hopefully fixed authorization in deployment scripts

--- build/nuget-push.ps1
+++ build/nuget-push.ps1
@@ -1,9 +1,12 @@
 $packagePath = $args[0]
 $source = $args[1]
-$apiKey = $args[2]
+$username = $args[2]
+$password = $args[3]
+
+dotnet nuget add source $source --name "GPR" --username $username --password $password
 
 $files = Get-ChildItem $packagePath\*.nupkg
 ForEach ($file in $files) {
   #echo $file.fullName
-  dotnet nuget push $file.fullName -s $source -k $apiKey 
+  dotnet nuget push $file.fullName -s "GPR" --skip-duplicate
 }
